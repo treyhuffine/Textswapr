ff2e71dce19b437ef94feee61dfeedf59a86f2cb
Trey Huffine <treyhuffine@gmail.com>
2015-07-11 17:22:46 -0700

    Remove console log from submit

--- public/javascripts/bundle.js
+++ public/javascripts/bundle.js
@@ -24,12 +24,10 @@ app
 
 app
 .controller("bookIndexCtrl", function($scope, Book) {
-  console.log("book index");
   $scope.books = [];
   Book.getBooks()
     .success(function(data) {
       $scope.books = data;
-      console.log($scope.books);
     })
     .catch(function(error) {
       console.log(error);
--- source/javascripts/controllers/bookIndexCtrl.js
+++ source/javascripts/controllers/bookIndexCtrl.js
@@ -1,11 +1,9 @@
 app
 .controller("bookIndexCtrl", function($scope, Book) {
-  console.log("book index");
   $scope.books = [];
   Book.getBooks()
     .success(function(data) {
       $scope.books = data;
-      console.log($scope.books);
     })
     .catch(function(error) {
       console.log(error);
