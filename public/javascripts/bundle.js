2bb7f160fc676274e4d10d2572795e6238689bd9
Trey Huffine <treyhuffine@gmail.com>
2015-07-11 19:10:49 -0700

    Delete main controller

--- public/javascripts/bundle.js
+++ public/javascripts/bundle.js
@@ -34,28 +34,6 @@ app
     });
 });
 
-app
-.controller("mainCtrl", function($scope, Book, User) {
-  $scope.addBook = function(book) {
-    Book.addBook(book)
-      .success(function(data){
-        console.log(data);
-      })
-      .catch(function(error) {
-        console.log(error);
-      });
-  };
-  $scope.getBooks = function() {
-    Book.getBooks()
-      .success(function(data) {
-        console.log(data);
-      })
-      .catch(function(error) {
-        console.log(error);
-      });
-  };
-});
-
 app.controller('navCtrl', function($scope, User) {
   $scope.setCurrentUser = function() {
     User.setCurrentUser();
