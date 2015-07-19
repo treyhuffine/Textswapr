074c14ca446f162fc9b07bc59579a7c33fee6c42
Trey Huffine <treyhuffine@gmail.com>
2015-07-19 16:09:43 -0700

    Change callback URL

--- config/auth.js
+++ config/auth.js
@@ -3,7 +3,7 @@ module.exports = {
   'twitterAuth' : {
     'consumerKey'       : process.env.CONSUMER_KEY,
     'consumerSecret'    : process.env.CONSUMER_SECRET,
-    'callbackURL'       : 'http://localhost:3000/auth/twitter/callback'
+    'callbackURL'       : 'http://textswapr.com/#//auth/twitter/callback'
   },
 
 };
