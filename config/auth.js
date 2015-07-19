454437b1b26ab7369845c46097c845aab9993dfe
Trey Huffine <treyhuffine@gmail.com>
2015-07-19 16:20:30 -0700

    Update front page styling

--- config/auth.js
+++ config/auth.js
@@ -3,7 +3,7 @@ module.exports = {
   'twitterAuth' : {
     'consumerKey'       : process.env.CONSUMER_KEY,
     'consumerSecret'    : process.env.CONSUMER_SECRET,
-    'callbackURL'       : 'http://textswapr.com/#/auth/twitter/callback'
+    'callbackURL'       : 'http://textswapr.com/auth/twitter/callback'
   },
 
 };
